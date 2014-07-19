6eede9dde6b8c283d7a48c35ca4e2b5985996215
Kevin Scott <kevin@scottlabsllc.com>
2014-07-19 13:31:43 -0400

    Working on getting record to work

--- Tuneful/ViewController.swift
+++ Tuneful/ViewController.swift
@@ -6,19 +6,96 @@
 //  Copyright (c) 2014 Kevin Scott. All rights reserved.
 //
 
-import UIKit
 
-class ViewController: UIViewController {
+import UIKit
+import Foundation
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioRecorderDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
+    
+    @IBAction func recordMe(sender: AnyObject) {
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        var err : NSError? = nil
+        
+        
+        if (audioSession.respondsToSelector("requestRecordPermission:")) {
+            AVAudioSession.sharedInstance().requestRecordPermission({(granted: Bool)-> Void in
+                if granted {
+                    println("granted")
+                    audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, error: &err)
+                    
+                    if (err) {
+                        println("There was an error setting the category")
+                        return
+                    }
+                    audioSession.setActive(true, error: &err)
+                    if (err) {
+                        println("There was an error setting the active")
+                        return
+                    }
+                    
+                    let path = NSHomeDirectory().stringByAppendingPathComponent("Documents")
+//                    println(path)
+                    
+                    
+                    let settings = [
+                        "AVFormatIDKey" : kAudioFormatLinearPCM,
+                        "AVSampleRateKey" : 44100.0,
+                        "AVNumberOfChannelsKey": 1,
+                        "AVLinearPCMIsFloatKey" : 0,
+                        "AvLinearPCMIsBigEndianKey" : 0,
+                        "AVLinearPCMBitDepthKey" : 16
+                        
+                    ]
+                    
+                    let now = NSDate().description
+                    
+                    let url = NSURL.fileURLWithPath(path+"/"+now+".caf")
+                    err = nil
+                    let recorder = AVAudioRecorder(URL: url, settings: settings, error: &err)
+                    if (err) {
+                        println("Shit on my face")
+                        return
+                    }
+                    
+                    recorder.meteringEnabled = true
+                    recorder.delegate = self
+                    
+                    recorder.prepareToRecord()
+                    
+                    if (!audioSession.inputAvailable ) {
+                        println("fuck my life")
+                        return
+                    }
+                    
+                    println(recorder.recordForDuration(3))
+                    
+                } else{
+                    println("not granted")
+                }
+            })
+            
+        }
+        
+        
+        
+        
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
